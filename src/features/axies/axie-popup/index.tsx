e442020aad2178ef08f8596d9517b5a946deafb2
Mohamed Saafan <201827254@std.sci.cu.edu.eg>
2021-09-07 22:54:40 +0200

    added checks to the filtering of good_breeder_creatures popup

--- src/features/axies/axie-popup/index.tsx
+++ src/features/axies/axie-popup/index.tsx
@@ -17,51 +17,54 @@ const AxiePopUp: React.FC<Props> = ({ axie, modal, setModal }) => {
 
   const renderAxies = () => {
     // filter the axies and render them
-    let parent1List = axie.parent1.split(",");
-    parent1List = parent1List.map((item) => item.trim());
-    let parent2List = axie.parent2.split(",");
-    parent2List = parent2List.map((item) => item.trim());
-    let siblingsList = axie.siblings.split(",");
-    siblingsList = siblingsList.map((item) => item.trim());
-    let childrenList = axie.children.split(",");
-    childrenList = childrenList.map((item) => item.trim());
+    let filteredArray: AxieType[] = [];
+    if (axie.parent1 && axie.parent2 && axie.siblings && axie.children) {
+      let parent1List = [axie.parent1];
+      parent1List = parent1List.map((item) => item.trim());
+      let parent2List = [axie.parent2];
+      parent2List = parent2List.map((item) => item.trim());
+      let siblingsList = axie.siblings.split(",");
+      siblingsList = siblingsList.map((item) => item.trim());
+      let childrenList = axie.children.split(",");
+      childrenList = childrenList.map((item) => item.trim());
 
-    const filteredArray = axies.filter((axie) => {
-      let shouldInclude = true;
-      if (!axie.good_for_breeding) {
-        return false;
-      }
-      let currnParent1List = axie.parent1.split(",");
-      currnParent1List = currnParent1List.map((item) => item.trim());
-      let currParent2List = axie.parent2.split(",");
-      currParent2List = currParent2List.map((item) => item.trim());
-      let currenSiblingsList = axie.siblings.split(",");
-      currenSiblingsList = currenSiblingsList.map((item) => item.trim());
-      let currentChildrenList = axie.children.split(",");
-      currentChildrenList = currenSiblingsList.map((item) => item.trim());
-      currenSiblingsList.forEach((sibling) => {
-        siblingsList.forEach((axieSibling) => {
-          if (sibling === axieSibling) shouldInclude = false;
+      filteredArray = axies.filter((axie) => {
+        let shouldInclude = true;
+        if (!axie.good_for_breeding) {
+          return false;
+        }
+        let currnParent1List = axie.parent1.split(",");
+        currnParent1List = currnParent1List.map((item) => item.trim());
+        let currParent2List = axie.parent2.split(",");
+        currParent2List = currParent2List.map((item) => item.trim());
+        let currenSiblingsList = axie.siblings.split(",");
+        currenSiblingsList = currenSiblingsList.map((item) => item.trim());
+        let currentChildrenList = axie.children.split(",");
+        currentChildrenList = currenSiblingsList.map((item) => item.trim());
+        currenSiblingsList.forEach((sibling) => {
+          siblingsList.forEach((axieSibling) => {
+            if (sibling === axieSibling) shouldInclude = false;
+          });
         });
-      });
-      currParent2List.forEach((parent) => {
-        parent2List.forEach((parent2) => {
-          if (parent === parent2) shouldInclude = false;
+        currParent2List.forEach((parent) => {
+          parent2List.forEach((parent2) => {
+            if (parent === parent2) shouldInclude = false;
+          });
         });
-      });
-      currnParent1List.forEach((parent) => {
-        parent1List.forEach((parent2) => {
-          if (parent === parent2) shouldInclude = false;
+        currnParent1List.forEach((parent) => {
+          parent1List.forEach((parent2) => {
+            if (parent === parent2) shouldInclude = false;
+          });
         });
-      });
-      currentChildrenList.forEach((child) => {
-        childrenList.forEach((axieChild) => {
-          if (child === axieChild) shouldInclude = false;
+        currentChildrenList.forEach((child) => {
+          childrenList.forEach((axieChild) => {
+            if (child === axieChild) shouldInclude = false;
+          });
         });
-      });
 
-      return shouldInclude;
-    });
+        return shouldInclude;
+      });
+    }
 
     return (
       <>
